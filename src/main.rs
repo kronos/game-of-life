680a568c19e248362ca977ce2982fa4269ced234
Samsonov Ivan <hronya@gmail.com>
2020-05-24 16:48:54 +0800

    Move out universe to a separate module

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use quicksilver::{
     run, Result, Settings, Window,
 };
 
+mod universe;
+
 use rand::Rng;
 use stopwatch::Stopwatch;
+use universe::Universe;
 
 static OFFSET: i32 = 2;
 static SIDE: i32 = 5;
@@ -14,61 +17,6 @@ static SPACE: i32 = 2;
 static WIDTH: i32 = 50;
 static HEIGHT: i32 = 50;
 static PAUSE_IN_MS: i64 = 50;
-
-pub struct Universe {
-    data: [Vec<bool>; 2],
-    width: usize,
-    height: usize,
-    n: usize,
-}
-
-impl Universe {
-    fn new(width: usize, height: usize) -> Universe {
-        return Universe {
-            data: [vec![false; width * height], vec![false; width * height]],
-            width,
-            height,
-            n: 0,
-        };
-    }
-
-    fn alive(&self, row: usize, col: usize) -> bool {
-        self.data[self.n][row * self.width + col]
-    }
-
-    fn refresh(&mut self) {
-        let mut idx = 0;
-        let w = self.width as i32;
-        let h = self.height as i32;
-        for i in 0..h {
-            for j in 0..w {
-                let mut cnt = if self.data[self.n][idx] { -1 } else { 0 };
-                for p in -1..=1 {
-                    let row = ((i + p + h) % h) as usize;
-                    for q in -1..=1 {
-                        let col = ((j + q + w) % w) as usize;
-                        if self.alive(row, col) {
-                            cnt += 1;
-                        }
-                    }
-                }
-                self.data[1 - self.n][idx] = (self.data[self.n][idx] && cnt == 2) || cnt == 3;
-                idx += 1
-            }
-        }
-        self.n = 1 - self.n;
-    }
-
-    fn init(&mut self, mut f: impl FnMut(usize, usize) -> bool) {
-        let mut idx = 0;
-        for i in 0..self.height {
-            for j in 0..self.width {
-                self.data[self.n][idx] = f(i, j);
-                idx += 1
-            }
-        }
-    }
-}
 
 fn main() {
     run(
@@ -87,8 +35,8 @@ fn main() {
 fn draw(universe: &Universe, gfx: &mut Graphics) {
     let mut y = OFFSET;
     gfx.clear(Color::BLACK);
-    for i in 0..universe.height {
-        for j in 0..universe.width {
+    for i in 0..universe.height() {
+        for j in 0..universe.width() {
             if universe.alive(i, j) {
                 let rect = Rectangle::new(
                     Vector::new(OFFSET + (SIDE + SPACE) * j as i32, y),
