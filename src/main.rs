857db13ca33f1fe43cd7440e85aeb9d3efc93589
Samsonov Ivan <hronya@gmail.com>
2020-05-24 15:31:08 +0800

    Enable stopping and starting process by pressing space

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use quicksilver::{
     geom::{Rectangle, Vector},
     graphics::{Color, Graphics},
-    input::Input,
+    input::{Event::KeyboardInput, Input, Key},
     run, Result, Settings, Window,
 };
 
 use rand::Rng;
 use stopwatch::Stopwatch;
+
+static OFFSET: i32 = 2;
+static SIDE: i32 = 5;
+static SPACE: i32 = 2;
+static WIDTH: i32 = 50;
+static HEIGHT: i32 = 50;
+static PAUSE_IN_MS: i64 = 50;
 
 pub struct Universe {
     data: [Vec<bool>; 2],
@@ -21,7 +28,7 @@ impl Universe {
             data: [vec![false; width * height], vec![false; width * height]],
             width,
             height,
-            n: 0
+            n: 0,
         };
     }
 
@@ -35,7 +42,7 @@ impl Universe {
         let h = self.height as i32;
         for i in 0..h {
             for j in 0..w {
-                let mut cnt =  if self.data[self.n][idx] { -1 } else { 0 };
+                let mut cnt = if self.data[self.n][idx] { -1 } else { 0 };
                 for p in -1..=1 {
                     let row = ((i + p + h) % h) as usize;
                     for q in -1..=1 {
@@ -67,6 +74,10 @@ fn main() {
     run(
         Settings {
             title: "Game of life",
+            size: Vector::new(
+                OFFSET * 2 + WIDTH * (SIDE + SPACE),
+                OFFSET * 2 + HEIGHT * (SIDE + SPACE),
+            ),
             ..Settings::default()
         },
         app,
@@ -74,48 +85,56 @@ fn main() {
 }
 
 fn draw(universe: &Universe, gfx: &mut Graphics) {
-    let offset = 2;
-    let side = 5;
-    let space = 2;
-    let mut y = offset;
+    let mut y = OFFSET;
     gfx.clear(Color::BLACK);
     for i in 0..universe.height {
-        y += side + space;
         for j in 0..universe.width {
             if universe.alive(i, j) {
                 let rect = Rectangle::new(
-                    Vector::new(offset + (side + space) * j as i32, y),
-                    Vector::new(side, side),
-                    
+                    Vector::new(OFFSET + (SIDE + SPACE) * j as i32, y),
+                    Vector::new(SIDE, SIDE),
                 );
                 gfx.fill_rect(&rect, Color::GREEN);
             }
         }
+        y += SIDE + SPACE;
     }
 }
 
 async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
-    let sw = Stopwatch::start_new();
-    let mut universe = Universe::new(145, 108);
+    let mut sw = Stopwatch::new();
+    let mut started = false;
+    let mut universe = Universe::new(WIDTH as usize, HEIGHT as usize);
     let mut rng = rand::thread_rng();
 
-    universe.init( move |i, j| -> bool {
-        rng.gen_range(0, i + j + 1) < (i + j)/2
-    });
+    universe.init(move |i, j| -> bool { rng.gen_range(0, i + j + 1) < (i + j) / 2 });
 
     draw(&universe, &mut gfx);
     gfx.present(&window)?;
     let mut cnt = 1;
 
     loop {
-        if sw.elapsed_ms() > 50*cnt {
+        if sw.elapsed_ms() > PAUSE_IN_MS * cnt {
             cnt += 1;
             universe.refresh();
             draw(&universe, &mut gfx);
             gfx.present(&window)?;
         }
 
-        while let Some(_) = input.next_event().await {
+        while let Some(event) = input.next_event().await {
+            match event {
+                KeyboardInput(key_event) => {
+                    if key_event.is_down() && key_event.key() == Key::Space {
+                        started = !started;
+                        if started {
+                            sw.start();
+                        } else {
+                            sw.stop();
+                        }
+                    }
+                }
+                _ => (), // println!("{:?}", event)
+            }
         }
     }
 }
